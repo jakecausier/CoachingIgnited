d3051da288fc029fdb00fd1d20754fd0838e52cc
Jake Causier <jake@purplemountmedia.com>
2019-06-14 22:49:58 +0100

    Fixed javascript error if post values didn't exist, preventing the video modal from opening

--- wp-content/themes/ignited/src/js/coaching-ignited.js
+++ wp-content/themes/ignited/src/js/coaching-ignited.js
@@ -28,32 +28,29 @@ $(function(){
 	    }
 	  }).done(function(response) {
 	  	var response = JSON.parse(response);
-	  	console.log(response);
 
 			if (response.status === 'success') {
 				
 				var post = response.data;
 				var meta = response.meta;
-
-				var title = post.post_title;
 				var description = post.post_content;
 				var trainer = '';
 				var location = '';
 				var embed = null;
 
-				if (meta.cignited_videos_trainer_name[0]) {
+				if (meta.cignited_videos_trainer_name) {
 					trainer = meta.cignited_videos_trainer_name[0];
 				}
-				if (meta.cignited_videos_trainer_location[0]) {
+				if (meta.cignited_videos_trainer_location) {
 					location = meta.cignited_videos_trainer_location[0];
 				}
-				if (meta.cignited_videos_videometa_embed_url[0]) {
+				if (meta.cignited_videos_videometa_embed_url) {
 		  		embed = 'https://www.youtube.com/embed/' + meta.cignited_videos_videometa_embed_url[0] + '?controls=0';
 		  	}
 
 		  	if (embed !== null) {
-			  	$('#video-modal #video-title').text(title);
-			  	$('#video-modal #video-subtitle').text(trainer + ' from ' + location);
+			  	$('#video-modal #video-title').text(trainer);
+			  	$('#video-modal #video-subtitle').text(location);
 			  	$('#video-modal #video-embed').attr('src', embed);
 			  	$('#video-modal #video-description').html(description);
 	    		$('#video-modal').modal('show');
